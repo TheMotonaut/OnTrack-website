ea30683add9e5ef3118e7be45799ea4f78df8400
TheMotonaut <simon-bjorklund@hotmail.com>
2022-03-19 16:19:39 +0100

    FIX7

--- html/retrieveLocation.php
+++ html/retrieveLocation.php
@@ -9,14 +9,21 @@ $db = new PDO("mysql:host=localhost;dbname=OnTrack", $user, $password);
 
 /* Check connection  */
 
+try{  
+    $result = $db->query("SELECT * FROM motorcycles WHERE owner = $_POST['owner']");
 
-$result = $db->query("SELECT * FROM motorcycles WHERE owner = $_POST['owner']");
+    $result_array = array();
+    foreach($result as $row){
+        array_push($result_array, $row);
+    }
 
-$result_array = array();
-foreach($result as $row){
-    array_push($result_array, $row);
+    header('Content-type: application/json');
+    echo json_encode($result_array);
+    $db->close();
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+
+
+$db->close();
 }
-
-header('Content-type: application/json');
-echo json_encode($result_array);
-$conn->close();
