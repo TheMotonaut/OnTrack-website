e51676e79702f885487c10d3a7d4a03ca757daf9
TheMotonaut <simon-bjorklund@hotmail.com>
2022-03-19 14:50:53 +0100

    Modular

--- html/index.php
+++ html/index.php
@@ -14,36 +14,14 @@ try{
     $db = new PDO("mysql:host=localhost;dbname=OnTrack", $user, $password);
     $result = $db->query("SELECT * FROM motorcycles WHERE owner = '$owner'");
 
-    foreach($result as $row)
+    foreach($result as $row){
     ?>
 
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Mototracker</title>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-    crossorigin=""/>
-
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-    crossorigin=""></script>
-
-    <link rel="stylesheet" href="css/style.css">
-
-    <script>
-        var latitude = <?php echo $row['latitude']; ?>;
-	    var longitude = <?php echo $row['longitude']; ?>;
-
-    </script>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="js/script.js" defer></script>
-</head>
 
 <?php
      }
+     include 'head.php';
      include 'body.php';
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
