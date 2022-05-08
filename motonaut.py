8a3ff5f08a2acc2fe25815c1406408bc2565be34
TheMotonaut <simonbjorklund98@gmail.com>
2022-05-08 15:32:09 +0000

    Yes

--- motonaut.py
+++ motonaut.py
@@ -28,8 +28,8 @@ import random
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAOUHFiCkhnOJG2N3CEX7bBKqs2BOjy50OyBRSAMR26+96Wm01WQ6wyHqqk5Uz5jxZNm2Un/ri/cZXpiO/wXD9WkCAwEAAQ'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://OnTrack:ILoveMotorcycles@localhost/OnTrack'
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 db = SQLAlchemy(app)
 
@@ -39,8 +39,8 @@ login_manager.init_app(app)
 
 app.config['MQTT_BROKER_URL'] = 'motonaut.se'  # use the free broker from HIVEMQ
 app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
-app.config['MQTT_USERNAME'] = 'OnTrack'  # set the username here if you need authentication for the broker
-app.config['MQTT_PASSWORD'] = 'fgh12qwe'  # set the password here if the broker demands authentication
+app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
+app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
 app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
 app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
 app.config['MQTT_CLEAN_SESSION'] = True
